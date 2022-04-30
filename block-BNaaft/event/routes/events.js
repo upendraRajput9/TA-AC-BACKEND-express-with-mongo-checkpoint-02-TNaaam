b3f03553d8bd147a8f0d930281db684af97804ec
upendra <upendrarajput9911@gmail.com>
2022-04-30 16:46:29 +0530

    changes

--- block-BNaaft/event/routes/events.js
+++ block-BNaaft/event/routes/events.js
@@ -26,8 +26,13 @@ router.post('/', (req, res, next) => {
 router.get('/', (req, res, next) => {
   var allCategories = []
   var allLocations = []
-
-  Event.find({}, (err, events) => {
+var query = {};
+if(req.query.category){
+  query.event_category=req.query.category
+}else if(req.query.location){
+  query.location=req.query.location
+}
+  Event.find(query,(err, events) => {
     if (err) return next(err)
 
     Event.distinct('event_category', (err, elem) => {
@@ -40,7 +45,6 @@ router.get('/', (req, res, next) => {
         categorie = allCategories
         allLocations = allLocations.flat()
         location = allLocations
-
         res.render('events', {
           list: events,
           categories: allCategories,
@@ -51,41 +55,40 @@ router.get('/', (req, res, next) => {
   })
 })
 
-//find by category
-router.get('/:category/category', (req, res, next) => {
-  let category = req.params.category
-  Event.find({ event_category: { $in: [category] } }, (err, events) => {
-    if (err) return next(err)
+//date
+router.get('/date',(req,res,next)=>{
+  var allCategories = []
+  var allLocations = []
+  console.log(req.query);
+  Event.find({$and:[{start_date:{$gte:req.query.startDate}},{end_date:{$lte:req.query.endDate}}]},(err,events)=>{
+    console.log(categorie,location)
+    if(err) return err
+    Event.distinct('event_category', (err, elem) => {
+      if (err) return next(err)
+      elem.forEach((str) => allCategories.push(str))
 
-    res.render('events', {
-      list: events,
-      categories: categorie,
-      locations: location,
+      Event.distinct('location', (err, loc) => {
+        if (err) return next(err)
+        allLocations.push(loc)
+        categorie = allCategories
+        allLocations = allLocations.flat()
+        location = allLocations
+        res.render('events', {
+          list: events,
+          categories: allCategories,
+          locations: allLocations,
+        })
+      })
     })
   })
 })
-
-//find by location
-router.get('/:location/location', (req, res, next) => {
-  let loc = req.params.location
-  Event.find({ location: loc }, (err, events) => {
-    if (err) return next(err)
-    console.log(location)
-    res.render('events', {
-      list: events,
-      categories: categorie,
-      locations: location,
-    })
-  })
-})
-
 //single event
 router.get('/:id', (req, res, next) => {
   let id = req.params.id
   Event.findById(id)
     .populate('remarks')
     .exec((err, event) => {
-      if (err) return next(err)
+      if (err) return next(err);
       res.render('singleEvent', { event: event })
     })
 })
@@ -93,10 +96,11 @@ router.get('/:id', (req, res, next) => {
 router.get('/:id/edit', (req, res, next) => {
   let id = req.params.id
   Event.findById(id, (err, event) => {
-    // event.start_date = event.start_date.toString().split('T')[0]
+    
     if (err) return next(err)
-    event.end_date = moment(event.end_date).format('DD/MM/YYYY').toString()
-    event.start_date = moment(event.start_date).format('DD/MM/YYYY').toString()
+    event.end_date=moment(event.end_date).format("DD/MM/YYYY").toString();
+     event.start_date=moment(event.start_date).format("DD/MM/YYYY").toString()
+    console.log(event.end_date,event.start_date)
     res.render('eventUpdate', {
       event: event,
     })
@@ -135,10 +139,16 @@ router.get('/:id/like', (req, res, next) => {
 //dislikes
 router.get('/:id/dislike', (req, res, next) => {
   let id = req.params.id
-  Event.findByIdAndUpdate(id, { $inc: { likes: -1 } }, (err, event) => {
+  Event.findById(id,(err,sinevent)=>{
     if (err) return next(err)
+else if(sinevent.likes>0){
+  Event.findByIdAndUpdate(sinevent._id, { $inc: { likes: -1 } }, (err, event) => {
+    if(err) return next(err)
     res.redirect('/events/' + id)
   })
+}
+  })
+  
 })
 router.post('/:id/remarks', async (req, res, next) => {
   let id = req.params.id
